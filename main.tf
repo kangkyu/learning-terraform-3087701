9a418184cd0d6b60f8603790bbff7d1b1ac65366
Kang-Kyu Lee <kangkyu1111@gmail.com>
2022-10-22 08:44:25 -0700

    vpc_id

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http in, allow everything out"
 
-  vpc = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
